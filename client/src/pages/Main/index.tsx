4f563336737978311fe08f9da2ea4908a825792c
oleksandrgrebinskij <roki000sasha@gmail.com>
2023-11-08 08:54:35 +0200

    test:sosy

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Main = () => {
     return (
         <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
-            <h1></h1>
+            <h1>asd</h1>
         </div>
     )
 }
